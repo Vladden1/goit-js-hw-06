78d5929c0fd54f0c21b9ecd4fd10ccfa3a76ca45
Vladden1 <denisenkovv1@gmail.com>
2022-11-16 12:11:59 +0100

    finish task-1 & task-2

--- js/task-01.js
+++ js/task-01.js
@@ -5,22 +5,30 @@ const numberOfCategories = item.length;
 console.log("Number of categories:", numberOfCategories);
 // Category: Animals
 // Elements: 4
-const title = document.querySelector("h2");
-console.log("Category:", title.textContent);
+const firstTitle = document.querySelector("h2");
+console.log("Category:", firstTitle.textContent);
 
+const categoryEl = document.querySelector("ul");
+const firstListEl = categoryEl.firstElementChild.querySelector("ul");
+const firstListElLength = firstListEl.children.length;
+
+console.log("Elemnets:", firstListElLength);
 // Category: Products
 // Elements: 3
+const secondTitle = categoryEl.querySelectorAll("h2")[1];
+console.log("Category:", secondTitle.textContent);
+
+const secondListEl = categoryEl.children[1].querySelector("ul");
+const secondListElLength = secondListEl.children.length;
+
+console.log("Elemnets:", secondListElLength);
 
 // Category: Technologies
 // Elements: 5
 
-//   Number of categories: 3
+const thirdTitle = categoryEl.querySelectorAll("h2")[2];
+console.log("Category:", thirdTitle.textContent);
 
-const listEl = [...item];
-console.log(listEl);
-// const itemEl = listEl.children;
-// console.log(itemEl);
-// for (const title of titles) {
-//   title.className = "title-animal";
-//   console.log(title);
-// }
+const thirdListEl = categoryEl.children[2].querySelector("ul");
+const thirdListElLength = thirdListEl.children.length;
+console.log("Elemnets:", thirdListElLength);
--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   "Herbs",
   "Condiments",
 ];
+const list = document.querySelector("#ingredients");
+
+ingredients.forEach((ingredient) => {
+  const firstItem = document.createElement("li");
+  const firstItemClass = firstItem.classList.add("item");
+
+  firstItem.textContent = ingredient;
+  list.append(firstItem);
+  console.log(firstItem);
+});
