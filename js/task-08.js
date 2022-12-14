36b7723d264a73935d8ea0acafb8a148190f28d4
Vladden1 <denisenkovv1@gmail.com>
2022-12-14 21:09:19 +0100

    finish task-08

---
+++ js/task-08.js
@@ -0,0 +1,21 @@
+const formEl = document.querySelector(".login-form");
+const userData = {};
+formEl.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("All fields must be filled!");
+  } else {
+    userData.email = email.value;
+    userData.password = password.value;
+
+    console.log(userData);
+    event.currentTarget.reset();
+  }
+}
