18c08e078d865fb2015137805e3d8a2414e69aa8
Vladden1 <denisenkovv1@gmail.com>
2022-12-19 14:17:43 +0100

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -2,7 +2,7 @@ const form = document.querySelector("#name-input");
 const text = document.querySelector("#name-output");
 
 form.addEventListener("input", () => {
-  if (form.value.length === 0) {
+  if (form.value.trim().length === 0) {
     text.textContent = "Anonymous";
   } else {
     text.textContent = form.value;
