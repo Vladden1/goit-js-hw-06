b999f6b173f07a780c3f2ee0947e4587b27ee91b
Vladden1 <denisenkovv1@gmail.com>
2022-12-23 11:49:22 +0100

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,23 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const divEL = document.querySelector("#controls");
+console.log(divEL);
+
+const inputEl = document.querySelector(`[type='number']`);
+console.log(inputEl);
+
+const btnCreateEL = document.querySelector(`[data-create]`);
+console.log(btnCreateEL);
+
+const btnDestroyEL = document.querySelector(`[data-destroy]`);
+console.log(btnDestroyEL);
+
+const divBoxesEl = document.querySelector("#boxes");
+
+console.log(divBoxesEl);
+
+btnCreateEL.addEventListener("click");
+
+btnDestroyEL.addEventListener("click");
